775e68528d95bf9334f77d89861792d5a2d5b13f
kiri <succhiello@gmail.com>
2017-02-08 22:40:26 +0900

    update version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requires = [
 
 setup(
     name='hieratic_dynamodb',
-    version='0.0.3',
+    version='0.0.4',
     description='dynamodb plugin for hieratic.',
     author='Satoshi Ebihara',
     author_email='kiri@succhiello.net',
